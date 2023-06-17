72878f80dbc51dac48207dc6b726acca8fe21afc
shiba_1234 <shibajisahu121@gmail.com>
2023-06-17 15:50:57 +0530

    Session - 3

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  { style: { color: "red" } },
-  "Namste"
+// const heading = React.createElement(
+//   "h1",
+//   { style: { color: "red" } },
+//   "Namste"
+// );
+
+const heading = <h1>Namste Shibaji</h1>;
+
+const Heading2 = () => (
+  <div>
+    {heading}
+    <div>Shibaji Sahu</div>
+  </div>
 );
+
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading2 />);
